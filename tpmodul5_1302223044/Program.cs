e04034dcca79ba808be8a03db8ed2bf9c9b3bde0
hafiiddd <hafid.akhyar0411@gmail.com>
2024-03-17 00:33:02 +0700

    Menambahkan class dataGeneric

--- tpmodul5_1302223044/Program.cs
+++ tpmodul5_1302223044/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Program;
+
+internal class Program
+{
+    public class haloGeneric
+    {
+        public void sapaUser<T>(T X)
+        {
+            Console.WriteLine("halo, " + X);
+        }
+    }
+    public class DataGeneric<Generic>
+    {
+        private Generic data;
+        public DataGeneric(Generic inputdata)
+        {
+            this.data = inputdata;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        haloGeneric halo = new haloGeneric();
+        halo.sapaUser("Hafid AlAkhyar");
+
+        Console.Write("NIM: ");
+        string nim = Console.ReadLine();
+        DataGeneric<string> data = new DataGeneric<string>(nim);
+        data.PrintData();
+    }
+}
